de9efac5bdd1d8cc41571ad7d6fb6bd74b396b88
Harry Jordan <harry@inquisitivesoftware.com>
2013-08-13 20:09:45 +0100

    Initial Commit

---
+++ Palettizer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Palettizer
+//
+//  Created by Harry Jordan on 13/08/2013.
+//  Copyright (c) 2013 Harry Jordan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
